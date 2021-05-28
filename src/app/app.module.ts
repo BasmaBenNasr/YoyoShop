0047613a5ce3277cac82759775b34ca783bc04c6
BasmaBenNasr <bennasrbasma97@gmail.com>
2021-05-28 01:14:18 +0100

    final version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
-import { HomePageModule } from './home-page/home-page.module';
 import { HttpClientModule } from '@angular/common/http';
+import { HomePageModule } from './home-page/home-page.module';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    
+  
+
 
   ],
   imports: [
@@ -19,7 +21,8 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule,
     AppRoutingModule,
     MDBBootstrapModule.forRoot(),
-    HomePageModule
+    HomePageModule,
+    
   ],
   schemas: [NO_ERRORS_SCHEMA],
   providers: [],
